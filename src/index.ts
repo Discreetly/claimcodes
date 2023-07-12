4b14c23ba29fd29801138e9ac8fbe5cc63129411
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2023-07-12 11:13:44 -0400

    version bump, removed default import

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import ClaimCodeManager, {
-    ClaimCodeStatus,
-    ClaimCodeStatusEnum,
-} from './manager'
-import type { ClaimCodeT, ClaimCodeSetsT } from './types'
+import ClaimCodeManager, { ClaimCodeStatus, ClaimCodeStatusEnum } from './manager';
+import type { ClaimCodeT, ClaimCodeSetsT } from './types';
 
-export default ClaimCodeManager
-export { ClaimCodeStatus, ClaimCodeStatusEnum }
-export type { ClaimCodeT, ClaimCodeSetsT }
+export { ClaimCodeManager, ClaimCodeStatus, ClaimCodeStatusEnum };
+export type { ClaimCodeT, ClaimCodeSetsT };
